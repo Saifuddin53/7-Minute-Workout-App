9d87562a89e64c3add875193a6e96c55d422b7d6
Saifuddin <adenwala32@gmail.com>
2024-05-09 17:15:49 +0530

    excerciseactivity top bar created

--- app/src/main/java/com/myprojects/a7minuteworkoutapp/ExcerciseActivity.kt
+++ app/src/main/java/com/myprojects/a7minuteworkoutapp/ExcerciseActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.myprojects.a7minuteworkoutapp.databinding.ActivityExcerciseBinding
 
 class ExcerciseActivity : AppCompatActivity() {
+    private var binding: ActivityExcerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_excercise)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityExcerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.toolBarExercise)
+
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding?.toolBarExercise?.setNavigationOnClickListener {
+            onBackPressed()
         }
     }
 }
