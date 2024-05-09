6ccb80011e8d7c382cef338f3165c5b0071aa5a4
Saifuddin <adenwala32@gmail.com>
2024-05-09 16:46:17 +0530

    updated in ui of mainactivity

--- app/src/main/java/com/myprojects/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/myprojects/a7minuteworkoutapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.myprojects.a7minuteworkoutapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +11,15 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val startButton = findViewById<Button>(R.id.button)
+        startButton.setOnClickListener {
+            Toast.makeText(
+                this,
+                "start button clicked",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
