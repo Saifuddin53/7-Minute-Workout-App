4d58eac878bb4909c2d8a1c5f508a0151112e55d
Saifuddin <adenwala32@gmail.com>
2024-06-25 16:58:44 +0530

    Exercises images imported

--- app/src/main/java/com/myprojects/a7minuteworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/myprojects/a7minuteworkoutapp/ExerciseActivity.kt
@@ -2,8 +2,10 @@ package com.myprojects.a7minuteworkoutapp
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Visibility
 import com.myprojects.a7minuteworkoutapp.databinding.ActivityExcerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
@@ -11,6 +13,8 @@ class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExcerciseBinding? = null
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,11 +53,39 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                setupExerciseView()
+            }
+
+        }.start()
+    }
+
+    private fun setupExerciseView() {
+        if(exerciseTimer != null)
+        {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        binding?.flExerciseProgressBar?.visibility = View.VISIBLE
+        setExerciseProgressBar()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding?.exerciseProgressBar?.progress = exerciseProgress
+        binding?.tvTitle?.text = "Exercise Name"
+        exerciseTimer = object: CountDownTimer(30000, 1000) {
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding?.exerciseProgressBar?.progress = 30 - exerciseProgress
+                binding?.tvExerciseTimer?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
                 Toast.makeText(
                     this@ExerciseActivity,
                     "Timer finished",
                     Toast.LENGTH_LONG
-                    ).show()
+                ).show()
             }
 
         }.start()
